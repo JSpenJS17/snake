7c297571e8bf55dd90ceb202aabff467dda7cbe4
PierceLaneKU <93334033+PierceLaneKU@users.noreply.github.com>
2023-02-04 16:58:51 -0600

    Added a quit state and prevented moving the reverse of the direction currently being traveled.

--- game.c
+++ game.c
@@ -6,15 +6,17 @@
 #include <string.h>
 
 
-#define TRUE 1
-#define FALSE 0
+#define FAIL 0
+#define SUCCESS 1
 #define SIZE_X 11
 #define SIZE_Y 11
 
 char key;
 COORD pos;
 char board[SIZE_Y][SIZE_X];
-
+int pos_list[100][2];
+int length;
+char vel = 'w';
 
 void delay(int ms){
     // Storing start time
@@ -46,31 +48,62 @@ void fill_board(){
     }
 }
 
-void move(){
+int move(){
     switch (key){
         case 'w':
-            if (pos.Y > 0)
+            if (pos.Y > 0 && vel != 's'){
                 pos.Y -= 1;
+                vel = key;
+            } else if (vel == 's'){
+                key = vel;
+                move();
+            } else {
+                return FAIL;
+            }
             break;
 
         case 's':
-            if (pos.Y < SIZE_Y - 1)
+            if (pos.Y < SIZE_Y - 1 && vel != 'w'){
                 pos.Y += 1;
+                vel = key;
+            } else if (vel == 'w'){
+                key = vel;
+                move();
+            } else {
+                return FAIL;
+            }
             break;
 
         case 'd':
-            if (pos.X < SIZE_X - 1)
+            if (pos.X < SIZE_X - 1 && vel != 'a'){
                 pos.X += 1;
+                vel = key;
+            } else if (vel == 'a'){
+                key = vel;
+                move();
+            } else {
+                return FAIL;
+            }
             break;
 
         case 'a':
-            if (pos.X > 0)
+            if (pos.X > 0 && vel != 'd'){
                 pos.X -= 1;
+                vel = key;
+            } else if (vel == 'd'){
+                key = vel;
+                move();
+            } else {
+                return FAIL;
+            }
             break;
 
         default:
+            key = vel;
+            move();
             break;
     }
+    return SUCCESS;
 }
 
 void init_board(){
@@ -83,22 +116,51 @@ void init_board(){
     }
 }
 
+void clear_board(){
+    int row, col;
+    COORD origin;
+    origin.X = 0;
+    origin.Y = 0;
+    HANDLE out = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleCursorPosition(out, origin);
+    for (row = 0; row < SIZE_Y; row++){
+        for (col = 0; col < SIZE_X; col++){
+            printf("  ");
+        }
+        printf("\n");
+    }
+    SetConsoleCursorPosition(out, origin);
+    printf("quit state\n");
+}
+
 int main(){
     system("cls");
+    int success;
+    pos.X = 5;
+    pos.Y = 5;
     fill_board();
     init_board();
+    delay(1000);
     while (1){
         if ( _kbhit() ){
             key = _getch();
             if (key == 'q')
                 return 0;
-            move();
+            success = move();
+            if (!success){
+                goto quit;
             }
-        else 
-            move();
+        } else {
+            success = move();
+            if (!success){
+                goto quit;
+            }
+        }
         fill_board();
         draw();
         delay(300);
     }
+    quit: clear_board();
+          return 0;
 }
 
