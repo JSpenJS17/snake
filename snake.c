35b44556b806590b1cc2a5a402602feb71a64baa
PierceLaneKU <93334033+PierceLaneKU@users.noreply.github.com>
2023-02-14 19:08:39 -0600

    v1.6 Release - Added options menu from 'esc' press on main menu - Cleaned up some unnecessary code - Fixed how the game resized windows when being run from terminal - Added functionality to the options     - Made the game fully resizeable

--- snake.c
+++ snake.c
@@ -1,6 +1,6 @@
 /*
  * Author: Pierce Lane
- * Last modified: 2/12/2023
+ * Last modified: 2/14/2023
  * Made to test the limits of my ability
  *
  */
@@ -17,21 +17,62 @@
 //define constants
 #define FAIL 0
 #define SUCCESS 1
-#define SIZE_X 11
-#define SIZE_Y 11
 #define UP 72
 #define LEFT 75
 #define DOWN 80
 #define RIGHT 77
 
 //define static variables
+int SIZE_X = 11;
+int SIZE_Y = 11;
 char key;
 COORD pos;
 int length;
-int snake_pos[SIZE_X * SIZE_Y + 1][3];
+int snake_pos[19 * 19 + 1][3];
 int del_pos[2];
 int apple_pos[2];
 char vel;
+int style = 0;
+
+int menu(char **options, int height_offset, int menu_length, int options_on,
+        char* header);
+
+void clear_screen(){
+    //function for clearing the std output screen
+    HANDLE out = GetStdHandle(STD_OUTPUT_HANDLE);
+    COORD origin;
+    origin.X = 0;
+    origin.Y = 0;
+    SetConsoleCursorPosition(out, origin);
+    printf("\033[0J");
+}
+
+char* centerText(char *text, int text_length, int buffer_len) { 
+    //return string text centered with spaces given the buffer length
+    int fieldWidth = buffer_len * 2 + 3;
+    int padlen = (fieldWidth - strlen(text)) / 2;
+    static char output[30];
+
+    int i;
+    for (i = 0; i < padlen; i++)
+        output[i] = ' ';
+    
+    for (i = i; i < text_length; i++)
+        output[i] = text[i];
+
+    for (i = i; i < padlen; i++)
+        output[i] = ' ';
+
+    snprintf(output, 30, "%*s%s%*s\n", padlen, "", text, padlen, "");
+
+    return output;
+}
+
+void resize_window(int x, int y){
+    char buffer[20];
+    snprintf(buffer, sizeof(buffer), "MODE %d,%d", x, y);
+    system(buffer);
+}
 
 int rand_lim(int limit) {
     //return a random integer between 0 and the limit.
@@ -58,6 +99,46 @@ void clrinp(){
     //helper function to clear input buffer
     int c;
     while ((c = getchar()) != '\n' && c != EOF);
+}
+
+void print_directional_segment(int seg_index){
+    switch (snake_pos[seg_index][2]){
+        case 'w':
+            printf("^");
+            break;
+        
+        case 's':
+            printf("v");
+            break;
+
+        case 'a':
+            printf("<");
+            break;
+
+        case 'd':
+            printf(">");
+            break;
+    }
+}
+
+void print_reverse_directional_segment(int seg_index){
+    switch (snake_pos[seg_index][2]){
+        case 'w':
+            printf("v");
+            break;
+        
+        case 's':
+            printf("^");
+            break;
+
+        case 'a':
+            printf(">");
+            break;
+
+        case 'd':
+            printf("<");
+            break;
+    }
 }
 
 void draw(int dead){
@@ -106,25 +187,31 @@ void draw(int dead){
             printf("\033[1;31m");
         
         //print the snake
-        //changes directions depending on the stored direction in the segment
-            switch (snake_pos[i][2]){
-                case 'w':
-                    printf("^");
-                    break;
-                
-                case 's':
-                    printf("v");
-                    break;
+        switch (style){
+            //changes directions depending on the 
+            //stored direction in the segment
+            case 0:
+                print_directional_segment(i);
+                break;
 
-                case 'a':
-                    printf("<");
-                    break;
+            case 1:
+                if (i == length - 1)
+                    print_directional_segment(i);
+                else if (i != 0)
+                    printf("~");
+                else
+                    print_reverse_directional_segment(i);
+                break;
 
-                case 'd':
-                    printf(">");
-                    break;
-            }
+            case 2:
+                if (i == length - 1)
+                    print_directional_segment(i);
+                else
+                    printf("~");
+                break;
         }
+    }
+        
     
     //restore color to black
     printf("\033[0m");
@@ -362,17 +449,6 @@ void init_board(){
     spawn_apple();
 }
 
-int is_good_input(char input, char *options){
-    //checks if char input is in char[] options. Returns 1 if so, 0 if not.
-    int i = 0;
-    while (*(options + i)){
-        if (input == options[i])
-            return 1;
-        i++;
-    }
-    return 0;
-}
-
 int key_in_wasd(char key){
     //returns 1 if the char given is in the char array "wasd"
     int i;
@@ -384,7 +460,7 @@ int key_in_wasd(char key){
     return FAIL;
 }
 
-void death_animation(int speed){
+void death_animation(){
     //handle the death wiping animation
     int i, j;
     COORD cursor_pos;
@@ -402,8 +478,9 @@ void death_animation(int speed){
 
     for (i = 0; i < SIZE_Y+2; i++){
         cursor_pos.Y = i;
+        SetConsoleCursorPosition(out, cursor_pos);
         printf("%s", str);    
-        delay(speed);
+        delay(50);
     }
 
     SetConsoleTextAttribute(out, 0x07);
@@ -412,12 +489,100 @@ void death_animation(int speed){
     SetConsoleCursorPosition(out, cursor_pos);
 }
 
-int menu(char **options, int height_offset, int menu_length){
+void options_menu(){
+    //create a page number tracker
+    int page = 0;
+    while (page < 2 && page >= 0){
+        //clear the screen every time
+        clear_screen();
+
+        //reset key every time we're here so that the errant \n key doesn't
+        //close the menu before we've selected what we want
+        key = 12;
+
+        //create a choice integer
+        int choice;
+
+        if (page == 0){
+            //do board size option on page 0
+            char *sizes[] = {"11x11 (default)", "13x13", "15x15",
+                            "17x17", "19x19", "Next", "Back"};
+            choice = menu(sizes, 2, 7, 0, "\033[1;32mBoard Size:\033[0m\n");
+            int choices[] = {11, 13, 15, 17, 19, SIZE_X, SIZE_X};
+            SIZE_X = choices[choice];
+            SIZE_Y = SIZE_X;
+            if (choice == 6)
+                //if choice was Back, go back a page
+                page--;
+            else
+                //if choice wasn't back, go forward
+                page++;
+
+        } else if (page == 1){
+            char *styles[] = {"<<<<<", "<~~~>", "<~~~~", "Next", "Back"};
+            choice = menu(styles, 2, 5, 0, "\033[1;35mSnake Style:\033[0m\n");
+            if (choice <= 2){
+                //if they chose a style
+                style = choice;
+                page++;
+            } else if (choice == 3)
+                //if they chose Next
+                page++;
+            else
+                //if they chose Back
+                page--;
+        }
+    }
+
+    //finally, after the user exited the options menu, clear the screen
+    clear_screen();
+}
+
+
+int main_menu(){
+    //choice is the player's choice of menu item (1, 2, 3, 4)
     int choice = 0;
+    //make the header, which is just some formatted text printed out at the
+    //beginning of the menu
+    char header[134] = "\033[1;32m";
+    strcat(header, centerText("S N A K E", 9, 11));
+    
+    //change color back and print menu stuff
+    strcat(header, "\n\033[0mMade by Pierce Lane\n\nWASD/Arrow Keys to move");
+    strcat(header, "\nEscape for options");
+    strcat(header, "\nEnter to select\n\nSelect a difficulty:\n");
+
+    //main menu options
+    char *options[] = {"\033[1;32mSlow\033[0m",
+                     "\033[1;33mFast\033[0m",
+                     "\033[1;31mInsane\033[0m",
+                     "\033[1;35mULTRA NIGHTMARE\033[0m"};
+    
+    //get the choice from the menu
+    choice = menu(options, 10, 4, 1, header);
+
+    //turn off the cursor
+    printf("\e[?25l");
+    
+    return choice;
+}
+
+int menu(char **options, int start_line, int menu_length, int options_on,
+        char *header){
+    //make sure the cursor is on
+    printf("\e[?25h");
+
+    //print the header
+    printf("%s", header);
+
+    //define menu choice number
+    int choice = 0;
+
+    //get terminal settings and stuff
     HANDLE out = GetStdHandle(STD_OUTPUT_HANDLE);
     COORD menu_pos;
     menu_pos.X = 0;
-    menu_pos.Y = height_offset - 1;
+    menu_pos.Y = start_line - 1;
 
     //print menu
     int i;
@@ -426,6 +591,7 @@ int menu(char **options, int height_offset, int menu_length){
         
     }
 
+    //do interactive menu part
     SetConsoleCursorPosition(out, menu_pos);
     //handle cursor position and selection
     while (key != 13) {
@@ -446,8 +612,22 @@ int menu(char **options, int height_offset, int menu_length){
                     else
                         choice = menu_length - 1;
                     break;
+                //if user hits escape
+                case 27:
+                    //if the options are enabled for this menu
+                    if (options_on){
+                        //display options menu
+                        options_menu();
+                        //after options are set, print header and menu again
+                        printf("%s", header);
+                        for (i = 0; i < menu_length; i++)
+                            printf("%d) %s\n", i+1, options[i]);
+                        //also set key != 13
+                        key = 12;
+                    }
+                    break;
             }
-        menu_pos.Y = height_offset + choice - 1;    
+        menu_pos.Y = start_line + choice - 1;    
         SetConsoleCursorPosition(out, menu_pos);
         }
     }
@@ -459,141 +639,159 @@ int menu(char **options, int height_offset, int menu_length){
 }
 
 int main(){
+    //get terminal information
+    HANDLE out = GetStdHandle(STD_OUTPUT_HANDLE);
+    //get the size of the screen before we mess it up
+    CONSOLE_SCREEN_BUFFER_INFO size;
+    GetConsoleScreenBufferInfo(out, &size);
+
     //resize window to perfect snakey size
-    system("MODE 25,13");
-    
-    //create a label for easy restarting and clear screen
-    start: system("cls");
-    //success determines if a move worked, speed is the delay in ms of the run
-    int success, speed;
-    //choice is the player's choice of menu item (1, 2, 3)
-    int choice = 0;
-    //set the length to 1 to begin with
-    length = 1;
-    //set key to 0 to begin with
-    key = 0;
-    //print title and my name and difficulty question
-    printf("\033[1;32m        S N A K E        \n\n");
-    printf("\033[0m");
-    printf("Made by Pierce Lane\n\n");
-    printf("WASD/Arrow Keys to move\nEnter to select\n\n");
-    printf("Select a difficulty:\n");
+    resize_window(11 * 2 + 3, 11 + 2);
+ 
+    //create the go_again while loop for if user wants to play again
+    int go_again = 1;
+    while (go_again){
+        //clear the screen
+        clear_screen();
 
-    //main menu options
-    char *options[] = {"\033[1;32mSlow\033[0m",
-                     "\033[1;33mFast\033[0m",
-                     "\033[1;31mInsane\033[0m",
-                     "\033[1;35mULTRA NIGHTMARE\033[0m"};
-    
-    //get the choice from the menu
-    choice = menu(options, 9, 4);
+        //variable success determines if a move worked
+        int success;   
 
-    //turn off the cursor
-    printf("\e[?25l");
-    
-    //set speed depending on what choice was
-    char mode[40];
-    switch (choice){
-        case 0:
-            strcpy(mode, "\033[1;32mSlow\033[0m");
-            speed = 300;
-            break;
-        case 1:
-            strcpy(mode, "\033[1;33mFast\033[0m");
-            speed = 200;
-            break;
-        case 2:
-            strcpy(mode, "\033[1;31mInsane\033[0m");
-            speed = 100;
-            break;
-        case 3:
-            strcpy(mode, "\033[1;35mULTRA NIGHTMARE\033[0m");
-            speed = 50;
-            break;
-    }
-    
-    //get a random seed
-    srand(time(NULL));
-    //set starting position
-    pos.X = 5;
-    pos.Y = 5;
-    //set starting position to be deleted on next draw
-    del_pos[0] = pos.X;
-    del_pos[1] = pos.Y;
-    //draw the board border and initial snake head
-    init_board();
+        //variable speed is the delay in ms of the run
+        int speed;
 
-    //wait until we get a WASD input then go
-    do {
-        while (!_kbhit());
-        key = _getch();
-        vel = key;
-    } while (!key_in_wasd(key));
+        //variable choice for storing menu option chosen by user
+        int choice;
 
-    //draw with an alive snek
-    draw(0);
+        //set the length to 1 to begin with
+        length = 1;
 
-    //main game loop
-    while (1){
-        //if a key was hit
-        if ( _kbhit() ){
-            //grab the key
-            key = _getch();
+        //set key to 0 to begin with
+        key = 0;
 
-            //move and store it's success state in success
-            success = move();
-            //if the move failed, draw the dead snek and break
-            if (!success){
-                draw(1);
+        //print main menu and get choice of speed from there
+        choice = main_menu();
+        //set speed depending on choice
+        char mode[33];
+        switch (choice){
+            case 0:
+                strcpy(mode, "\033[1;32mSlow\033[0m");
+                speed = 300;
                 break;
-            }
-
-        //if no key was hit
-        } else {
-            //move without updating key pressed, same as before
-            success = move();
-            if (!success){
-                draw(1);
+            case 1:
+                strcpy(mode, "\033[1;33mFast\033[0m");
+                speed = 200;
                 break;
-            }
+            case 2:
+                strcpy(mode, "\033[1;31mInsane\033[0m");
+                speed = 100;
+                break;
+            case 3:
+                strcpy(mode, "\033[1;35mULTRA NIGHTMARE\033[0m");
+                speed = 50;
+                break;
         }
-        //update display with an alive snek
-        draw(0);
-        //delay depending on chosen speed
-        delay(speed);
-    }
-    //after death, play death animation
-    delay(500);
-    death_animation(speed);
-    //make sure the cursor is on
-    printf("\e[?25h");
-    printf("\033[1;31m        Game Over        \033[0m\n\n");
-    printf("Mode: %s\n", mode);
-    printf("Final Length: %d\n\n", length);
-    printf("Play again?\n");
 
-    char *yesno[] = {"\033[1;32mYes\033[0m", "\033[1;31mNo\033[0m"};
-    int go_again = !menu(yesno, 7, 2);
-    printf("go again: %d", go_again);
-    //ask the player to play again
-    if (go_again){
-        //if they say yes, restart
-        goto start;
+        //resize the window to set dimensions from options menu
+        SIZE_Y = SIZE_X;
+        resize_window(SIZE_X * 2 + 3, SIZE_Y + 2);
+
+        //get a random seed
+        srand(time(NULL));
+
+        //set starting position
+        pos.X = SIZE_X / 2;
+        pos.Y = SIZE_Y / 2;
+
+        //set starting position to be deleted on next draw
+        del_pos[0] = pos.X;
+        del_pos[1] = pos.Y;
+
+        //draw the board border and initial snake head
+        init_board();
+
+        //wait until we get a WASD input then go
+        do {
+            while (!_kbhit());
+            key = _getch();
+            vel = key;
+        } while (!key_in_wasd(key));
+
+        //draw with an alive snek
+        draw(0);
+
+        //main game loop
+        while (1){
+            //if a key was hit
+            if ( _kbhit() ){
+                //grab the key
+                key = _getch();
+
+                //move and store it's success state in success
+                success = move();
+                //if the move failed, draw the dead snek and break
+                if (!success){
+                    draw(1);
+                    break;
+                }
+
+            //if no key was hit
+            } else {
+                //move without updating key pressed, same as before
+                success = move();
+                if (!success){
+                    draw(1);
+                    break;
+                }
+            }
+            //update display with an alive snek
+            draw(0);
+            //delay depending on chosen speed
+            delay(speed);
+        }
+        //after death, play death animation
+        delay(500);
+        death_animation();
+        
+        //resize the window back to normal
+        resize_window(11 * 2 + 3, 11 + 2);
+
+        //change color and print Game Over centered
+        printf("\033[1;31m");
+        printf("%s", centerText("Game Over", 9, 11));;
+
+        //change color back and print stats + menu options
+        printf("\033[0m\n");
+        printf("Mode: %s\n", mode);
+        printf("Board Size: %dx%d\n", SIZE_X, SIZE_Y);
+        printf("Final Length: %d\n\n", length);
+        printf("Play again?\n");
+
+        //ask the player if they'd like to play again
+        char *yesno[] = {"\033[1;32mYes\033[0m", "\033[1;31mNo\033[0m"};
+        //go_again doesn't need any header option because it's all already
+        //printed. this saves some memory, not having to store the output str.
+        go_again = !menu(yesno, 8, 2, 0, "");
     }
+        
 
     //otherwise, thank the user for playing and do credits
-    system("cls");
+    clear_screen();
     printf("\e[?25l"); //remove the cursor again
-    printf("\033[1;32m   Thanks for playing!   \n\n");
-    printf("\033[0m");
+    //change color
+    printf("\033[1;32m");
+    //print Thanks for playing! centered
+    printf("%s", centerText("Thanks for playing!", 19, 11));
+    //change color back
+    printf("\n\033[0m");
     printf("Made by:\n");
     printf("Pierce Lane\n\n");
     printf("Special thanks to:\n");
     printf("Landrea\n\n");
     printf("Press any key to exit...");
     getch();
-    //also resize their window to a usable size for if they ran this in terminal
-    system("MODE 120,30");
+    //also resize their window to original size
+    resize_window(size.dwMaximumWindowSize.X, size.dwMaximumWindowSize.Y-1);
     return 0;
 }
 
